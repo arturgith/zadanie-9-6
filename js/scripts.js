dfbefde74b053e1f39a90f673827baf7bcfa27a8
Artur Marcinkowski <arturmarcinkowski@interia.pl>
2018-03-30 15:30:49 +0200

    Add files and code for exercise 9.6

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+var list = document.getElementById('list'),
+	add = document.getElementById('addElem');
+
+add.addEventListener('click', function(addElem) {
+	var element = document.createElement('li');
+	element.innerHTML = 'item' + document.getElementsByTagName('li').length;
+	list.appendChild(element);
+});
